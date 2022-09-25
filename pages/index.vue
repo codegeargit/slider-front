15dbbbcd4c4485c343b7fbc78315800d88464bdd
thom <dukeom@gmail.com>
2022-09-25 15:28:28 +0900

    kakao login 추가

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,25 @@
 <template>
-  <Tutorial />
+  <a @click="loginWithKakao">
+    <img src="/kakao_login.png"/>
+  </a>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  methods: {
+    kakaoInit() {
+      Kakao.init('497bcfa986482f48ba2ee19b185ca2b0')
+      Kakao.isInitialized()
+    },
+    async loginWithKakao() {
+      await Kakao.Auth.authorize({
+        redirectUri: `${window.location.origin}/kakao-callback`
+      })
+    }
+  },
+  mounted(){
+    this.kakaoInit()
+  }
 }
 </script>
---
+++ pages/kakao-callback.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>로그인중....</div>
+</template>
+
+<script>
+  export default {
+    async mounted() {
+      try{
+        if(!this.$route.query.code){
+          return this.$route.push('/')
+        }
+        console.log(`code : ${this.$route.query.code}`)
+        const body = {
+          code: this.$route.query.code,
+          domain: window.location.orgin
+        }
+        const response = await this.$axios.$post(
+            '/auth/login', body, {}
+        )
+        console.log(response)
+      }catch(error){
+        console.log(error)
+      }
+    }
+  }
+</script>
